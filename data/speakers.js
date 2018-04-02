ed09fb7e40e741db23f18f7ad6d695807b1a4a97
Ilyas F <it83.mohammed@gmail.com>
2018-04-02 20:19:51 +0530

    Added Speaker Info

--- data/speakers.js
+++ data/speakers.js
@@ -11,8 +11,8 @@ var featuredSpeakerData = {
     { name: "Deepak Singh Dhami", imageURL: "assets/images/speakers/dhami.jpg", title: "" },
     { name: "Ilyas", imageURL: "assets/images/speakers/ilyas.png", title: " " },
     { name: "Anoop Chandran Nair", imageURL: "assets/images/speakers/anoop.jpg", title: "" },
-    { name: "Swaminathan Vetri", imageURL: "assets/images/speakers/swami.jpg", title: "" }
-   
+    { name: "Swaminathan Vetri", imageURL: "assets/images/speakers/swami.jpg", title: "" },
+    { name: "Anand Gothe", imageURL: "assets/images/speakers/anand.jpg", title: "" }
   ]
 };
 
