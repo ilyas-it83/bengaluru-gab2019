96f7a16b60261fa910aed9d64d69d4c668edff53
Ilyas F <it83.mohammed@gmail.com>
2018-04-08 23:51:24 +0530

    Speakers Updated

--- data/agenda.js
+++ data/agenda.js
@@ -1,21 +1,20 @@
 
 var day1AgendaData={ 
     dev:[
-        { title:"Keynote Address",speaker:"Ilyas",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"09.00 AM - 09.45 AM"},
+        { title:"Keynote Address",speaker:"Ilyas",imageURL: "assets/images/speakers/wilhelm.jpeg",desc:"", time:"09.00 AM - 09.45 AM"},
         { title:"Keynote Address",speaker:"Swaminathan Vetri",imageURL:"assets/images/speakers/swami.jpg",desc:"Learn how to easily build, debug, deploy, diagnose, and manage multi-platform, scalable apps and services using the rich set of developer tools", time:"10.00 AM - 10.45 AM"},
         { title:"Break",speaker:"",imageURL: "assets/images/coffee.png",desc:"", time:"10.45 AM - 11.00 AM"},
         { title:"Building Secure Web Applications with Microsoft Technologies",imageURL: "assets/images/speakers/default-speaker.png", time:"11.00 AM - 11.45 AM"},
-        { title:"Azure for Mobile Developers",speaker:"Nish Anil",imageURL:"assets/images/speakers/Nish.jpg",desc:"", time:"12.00 AM - 12.45 AM"},
+        { title:"Go Mobile with Xamarin and Azure",speaker:"Nish Anil",imageURL:"assets/images/speakers/Nish.jpg",desc:"", time:"12.00 AM - 12.45 AM"},
         { title:"Lunch",speaker:"",imageURL:"assets/images/food.png",desc:"", time:"1.00 PM - 2.00 PM"},
         { title:"Demystifying the APIs in Cosmos DB ",speaker:"Mohit Chhabra",imageURL:"assets/images/speakers/mohit.jpg",desc:"", time:"02.00 PM - 02.45 PM"},
-        { title:"There is a service for that!",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"03.00 PM -3.45 PM"},
+        { title:"To be Updated",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"03.00 PM -3.45 PM"},
         { title:"Break",imageURL: "assets/images/coffee.png", time:"03.45 PM - 04.00 PM"},
         { title:"Building Planet Scale APIs with Service Fabric",imageURL: "assets/images/speakers/anand.jpg",desc:"Azure Service Fabric makes it easy to deploy and manage your microservices on cloud or on-premise. It's the underlying technology used by lot of Azure services as well. We'll cover the basics and I'll squeeze in a demo too", time:"04.00 PM - 04.45 PM"},
-        { title:"TBA",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"05.00 PM - 05.45 PM"},
-        { title:"Conclusion",imageURL: "assets/images/speakers/default-speaker.png", time:"05.45 PM - 06.00 PM"},
+        { title:"Conclusion & Vote of Thanks",imageURL: "assets/images/speakers/default-speaker.png", time:"05.00 PM - 05.30 PM"}
     ],
     devOSS:[
-        { title:"Keynote Address",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"09.00 AM - 09.45 AM"},
+        { title:"Keynote Address",speaker:"Ilyas",imageURL: "assets/images/speakers/wilhelm.jpeg",desc:"", time:"09.00 AM - 09.45 AM"},
         { title:"Getting Started with ASP.NET Core 2.0",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"10.00 AM - 10.45 AM"},
         { title:"Break",imageURL: "assets/images/coffee.png", time:"10.45 AM - 11.00 AM"},
         { title:"Getting Started with Angular 5",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"11.00 AM - 11.45 AM"},
@@ -25,25 +24,23 @@ var day1AgendaData={
         { title:"TBA",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"03.00 PM -3.45 PM"},
         { title:"Break",imageURL: "assets/images/coffee.png", time:"03.45 PM - 04.00 PM"},
         { title:"TBA",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"04.00 PM - 04.45 PM"},
-        { title:"TBA",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"05.00 PM - 05.45 PM"},
-        { title:"Conclusion",imageURL: "assets/images/speakers/default-speaker.png", time:"05.45 PM - 06.00 PM"},
+        { title:"Conclusion & Vote of Thanks",imageURL: "assets/images/speakers/default-speaker.png", time:"05.00 PM - 05.30 PM"}
     ],
     itPro:[
-        { title:"Keynote Address",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"09.00 AM - 09.45 AM"},
-        { title:"Design & Secure Your Cloud Infrastructure",speaker:"",imageURL: "assets/images/speakers/Sarvesh.jpg",desc:"", time:"10.00 AM - 10.45 AM"},
+        { title:"Keynote Address",speaker:"Ilyas",imageURL: "assets/images/speakers/wilhelm.jpeg",desc:"", time:"09.00 AM - 09.45 AM"},
+        { title:"Design & Secure Your Cloud Infrastructure",speaker:"",imageURL: "assets/images/speakers/Sarvesh.jpg",desc:"A case study of migrate your large business critical application to Microsoft Azure Cloud Platform (co-Presenting with Ajay Kakkar)", time:"10.00 AM - 10.45 AM"},
         { title:"Break", imageURL: "assets/images/coffee.png",time:"10.45 AM - 11.00 AM"},
-        { title:"SCCM with Azure PaaS services (CDP and CMG) - Troubleshooting Tips",speaker:"",imageURL: "assets/images/speakers/Sudeesh.jpg",desc:"", time:"11.00 AM - 11.45 AM"},
+        { title:"SCCM with Azure PaaS services (CDP and CMG) - Troubleshooting Tips",speaker:"",imageURL: "assets/images/speakers/Sudeesh.jpg",desc:"Cloud Management Gateway and Cloud Distribution Point Troubleshooting", time:"11.00 AM - 11.45 AM"},
         { title:"Azure Automation with PowerShell",speaker:"",imageURL: "assets/images/speakers/dhami.jpg",desc:"", time:"12.00 AM - 12.45 AM"},
         { title:"Lunch",imageURL:"assets/images/food.png", time:"1.00 PM - 2.00 PM"},
-        { title:"Modern Management with Intune – Migrate the Group Policy to CSP",speaker1:"",speaker2:"",imageURL: "assets/images/speakers/vimal.jpeg",desc:"", time:"02.00 PM - 02.45 PM"},
-        { title:"Azure Backup and Restore Best Practices",speaker:"",imageURL: "assets/images/speakers/Adil.jpg",desc:"", time:"03.00 PM -3.45 PM"},
+        { title:"Modern Management with Intune – Migrate the Group Policy to CSP",speaker1:"",speaker2:"",imageURL: "assets/images/speakers/vimal.jpeg",desc:"Modern Management with Intune – Migrate the Group Policy to CSP", time:"02.00 PM - 02.45 PM"},
+        { title:"Azure Backup and Restore Best Practices",speaker:"",imageURL: "assets/images/speakers/Adil.jpg",desc:"As organizations go with cloud strategy, one of the most compelling use case is to use as Disaster Recovery solution because of the economics and pay as you go model. ", time:"03.00 PM -3.45 PM"},
         { title:"Break",imageURL: "assets/images/coffee.png", time:"03.45 PM - 04.00 PM"},
-        { title:"Windows 10 co management benefits and real-world experience",speaker:"",imageURL: "assets/images/speakers/Rajul.jpg",desc:"", time:"04.00 PM - 04.45 PM"},
-        { title:"To be added",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"05.00 PM - 05.45 PM"},
-        { title:"Conclusion",imageURL: "assets/images/speakers/default-speaker.png", time:"05.45 PM - 06.00 PM"},
+        { title:"Windows 10 co management benefits and real-world experience",speaker:"",imageURL: "assets/images/speakers/Rajul.jpg",desc:"Co-Management delivers a bridge that simplifies planning and reduces the risks as organizations transition workloads from traditional to modern management", time:"04.00 PM - 04.45 PM"},
+        { title:"Conclusion & Vote of Thanks",imageURL: "assets/images/speakers/default-speaker.png", time:"05.00 PM - 05.30 PM"}
     ],
     sharePoint:[
-        { title:"Keynote Address",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"09.00 AM - 09.45 AM"},
+        { title:"Keynote Address",speaker:"Ilyas",imageURL: "assets/images/speakers/wilhelm.jpeg",desc:"", time:"09.00 AM - 09.45 AM"},
         { title:"How to make your Azure Deployments secure",speaker:"",imageURL: "assets/images/speakers/Sarvesh.jpg",desc:"", time:"10.00 AM - 10.45 AM"},
         { title:"Break", imageURL: "assets/images/coffee.png",time:"10.45 AM - 11.00 AM"},
         { title:"SCCM with Azure PaaS services (CDP and CMG) - Troubleshooting Tips",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"11.00 AM - 11.45 AM"},
@@ -53,22 +50,20 @@ var day1AgendaData={
         { title:"Azure Backup and Restore Best Practices",speaker:"",imageURL: "assets/images/speakers/Adil.jpg",desc:"", time:"03.00 PM -3.45 PM"},
         { title:"Break",imageURL: "assets/images/coffee.png", time:"03.45 PM - 04.00 PM"},
         { title:"Windows 10 co management benefits and real-world experience",speaker:"",imageURL: "assets/images/speakers/Rajul.jpg",desc:"", time:"04.00 PM - 04.45 PM"},
-        { title:"To be added",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"05.00 PM - 05.45 PM"},
-        { title:"Conclusion",imageURL: "assets/images/speakers/default-speaker.png", time:"05.45 PM - 06.00 PM"},
+        { title:"Conclusion & Vote of Thanks",imageURL: "assets/images/speakers/default-speaker.png", time:"05.00 PM - 05.30 PM"}
     ],
     office:[
-        { title:"Accelerate your digital transformation with SharePoint Online , OneDrive for Business and Microsoft Flow", speaker:"Vignesh Ganesan",imageURL:"assets/images/speakers/default-speaker.png",desc:"In this session we will learn about how the new and upcoming SharePoint , OneDrive & Microsoft Flow innovations empower people and organizations to share with confidence, transform business processes, engage employees, and harness collective knowledge from anywhere, on virtually any device. We will have also do a DEMO and guide the audiences with a hands-on lab as a part of this session", time:"09.00 AM - 09.45 AM"},
-        { title:"What story does your project tell?",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"10.00 AM - 10.45 AM"},
+        { title:"Keynote Address",speaker:"Ilyas",imageURL: "assets/images/speakers/wilhelm.jpeg",desc:"", time:"09.00 AM - 09.45 AM"},
+        { title:"Build intelligent azure bot using QnA Maker & Luis integrate with SharePoint Online",speaker:"",imageURL: "assets/images/speakers/manoj.jpg",desc:"", time:"10.00 AM - 10.45 AM"},
         { title:"Break",imageURL: "assets/images/coffee.png", time:"10.45 AM - 11.00 AM"},
-        { title:"Implementing smart strategies to protect your data within Office 365",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"11.00 AM - 11.45 AM"},
-        { title:"Build your PowerApps Bot in 45 minutes",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"12.00 AM - 12.45 AM"},
+        { title:"Accelerate your digital transformation with SharePoint Online , OneDrive for Business and Microsoft Flow", speaker:"Vignesh Ganesan",imageURL:"assets/images/speakers/vignesh.jpg",desc:"In this session we will learn about how the new and upcoming SharePoint , OneDrive & Microsoft Flow innovations empower people and organizations to share with confidence, transform business processes, engage employees, and harness collective knowledge from anywhere, on virtually any device. We will have also do a DEMO and guide the audiences with a hands-on lab as a part of this session", time:"11.00 AM - 11.45 AM"},
+        { title:"Develop workflows using visual studio 2015 and deploy to SharePoint online",speaker:"",imageURL: "assets/images/speakers/Bijay.jpg",desc:"", time:"12.00 AM - 12.45 AM"},
         { title:"Lunch",imageURL:"assets/images/food.png", time:"1.00 PM - 2.00 PM"},
-        { title:"Task management using Planner and Flow",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"02.00 PM - 02.45 PM"},
-        { title:"Building your first PowerApps Bot",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"03.00 PM -3.45 PM"},
+        { title:"To be added",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"02.00 PM - 02.45 PM"},
+        { title:"To be added",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"03.00 PM -3.45 PM"},
         { title:"Break",imageURL: "assets/images/coffee.png", time:"03.45 PM - 04.00 PM"},
-        { title:"Introducing Microsoft Teams and Building your Chat Bot in 15 minutes",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"04.00 PM - 04.45 PM"},
-        { title:"Modern Workplace with Microsoft 365",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"05.00 PM - 05.45 PM"},
-        { title:"Conclusion",imageURL: "assets/images/speakers/default-speaker.png", time:"05.45 PM - 06.00 PM"},
+        { title:"To be added",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"04.00 PM - 04.45 PM"},
+        { title:"Conclusion & Vote of Thanks",imageURL: "assets/images/speakers/default-speaker.png", time:"05.00 PM - 05.30 PM"}
     ]
 
 
