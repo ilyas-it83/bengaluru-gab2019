e20cae609f7a39c40895ea26df2c7987948f273e
Ilyas F <it83.mohammed@gmail.com>
2018-04-09 00:02:08 +0530

    Updated Speakers

--- data/agenda.js
+++ data/agenda.js
@@ -2,16 +2,16 @@
 var day1AgendaData={ 
     dev:[
         { title:"Keynote Address",speaker:"Ilyas",imageURL: "assets/images/speakers/wilhelm.jpeg",desc:"", time:"09.00 AM - 09.45 AM"},
-        { title:"Keynote Address",speaker:"Swaminathan Vetri",imageURL:"assets/images/speakers/swami.jpg",desc:"Learn how to easily build, debug, deploy, diagnose, and manage multi-platform, scalable apps and services using the rich set of developer tools", time:"10.00 AM - 10.45 AM"},
+        { title:"To be added",speaker:"",imageURL:"assets/images/speakers/default-speaker.png",desc:"To be added", time:"10.00 AM - 10.45 AM"},
         { title:"Break",speaker:"",imageURL: "assets/images/coffee.png",desc:"", time:"10.45 AM - 11.00 AM"},
-        { title:"Building Secure Web Applications with Microsoft Technologies",imageURL: "assets/images/speakers/default-speaker.png", time:"11.00 AM - 11.45 AM"},
-        { title:"Go Mobile with Xamarin and Azure",speaker:"Nish Anil",imageURL:"assets/images/speakers/Nish.jpg",desc:"", time:"12.00 AM - 12.45 AM"},
+        { title:"To be added",imageURL: "assets/images/speakers/default-speaker.png",desc:"To be added", time:"11.00 AM - 11.45 AM"},
+        { title:"Go Mobile with Xamarin and Azure",speaker:"Nish Anil",imageURL:"assets/images/speakers/Nish.jpg",desc:"Learn to build intelligent apps with scalable mobile backends using Xamarin and Azure", time:"12.00 AM - 12.45 AM"},
         { title:"Lunch",speaker:"",imageURL:"assets/images/food.png",desc:"", time:"1.00 PM - 2.00 PM"},
-        { title:"Demystifying the APIs in Cosmos DB ",speaker:"Mohit Chhabra",imageURL:"assets/images/speakers/mohit.jpg",desc:"", time:"02.00 PM - 02.45 PM"},
-        { title:"To be Updated",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"03.00 PM -3.45 PM"},
+        { title:"Demystifying the APIs in Cosmos DB ",speaker:"Mohit Chhabra",imageURL:"assets/images/speakers/mohit.jpg",desc:"To be added", time:"02.00 PM - 02.45 PM"},
+        { title:"Microservices are easy with Azure Service Fabric",speaker:"",imageURL: "assets/images/speakers/anand.jpg",desc:"Azure Service Fabric makes it easy to code, deploy and manage your Microservices on cloud or on-premise. It's the underlying technology used by lot of Microsoft services as well. We'll cover the basics and I'll squeeze in some demos too", time:"03.00 PM -3.45 PM"},
         { title:"Break",imageURL: "assets/images/coffee.png", time:"03.45 PM - 04.00 PM"},
-        { title:"Building Planet Scale APIs with Service Fabric",imageURL: "assets/images/speakers/anand.jpg",desc:"Azure Service Fabric makes it easy to deploy and manage your microservices on cloud or on-premise. It's the underlying technology used by lot of Azure services as well. We'll cover the basics and I'll squeeze in a demo too", time:"04.00 PM - 04.45 PM"},
-        { title:"Conclusion & Vote of Thanks",imageURL: "assets/images/speakers/default-speaker.png", time:"05.00 PM - 05.30 PM"}
+        { title:"To be added",imageURL: "assets/images/speakers/murugan.png",desc:"To be added", time:"04.00 PM - 04.45 PM"},
+        { title:"Conclusion & Vote of Thanks",imageURL: "assets/images/speakers/swami.jpg", time:"05.00 PM - 05.30 PM"}
     ],
     devOSS:[
         { title:"Keynote Address",speaker:"Ilyas",imageURL: "assets/images/speakers/wilhelm.jpeg",desc:"", time:"09.00 AM - 09.45 AM"},
@@ -54,10 +54,10 @@ var day1AgendaData={
     ],
     office:[
         { title:"Keynote Address",speaker:"Ilyas",imageURL: "assets/images/speakers/wilhelm.jpeg",desc:"", time:"09.00 AM - 09.45 AM"},
-        { title:"Build intelligent azure bot using QnA Maker & Luis integrate with SharePoint Online",speaker:"",imageURL: "assets/images/speakers/manoj.jpg",desc:"", time:"10.00 AM - 10.45 AM"},
+        { title:"Build intelligent azure bot using QnA Maker & Luis integrate with SharePoint Online",speaker:"",imageURL: "assets/images/speakers/manoj.jpg",desc:"Build intelligent azure bot using QnA Maker & Luis integrate with SharePoint Online", time:"10.00 AM - 10.45 AM"},
         { title:"Break",imageURL: "assets/images/coffee.png", time:"10.45 AM - 11.00 AM"},
         { title:"Accelerate your digital transformation with SharePoint Online , OneDrive for Business and Microsoft Flow", speaker:"Vignesh Ganesan",imageURL:"assets/images/speakers/vignesh.jpg",desc:"In this session we will learn about how the new and upcoming SharePoint , OneDrive & Microsoft Flow innovations empower people and organizations to share with confidence, transform business processes, engage employees, and harness collective knowledge from anywhere, on virtually any device. We will have also do a DEMO and guide the audiences with a hands-on lab as a part of this session", time:"11.00 AM - 11.45 AM"},
-        { title:"Develop workflows using visual studio 2015 and deploy to SharePoint online",speaker:"",imageURL: "assets/images/speakers/Bijay.jpg",desc:"", time:"12.00 AM - 12.45 AM"},
+        { title:"Develop workflows using visual studio 2015 and deploy to SharePoint online",speaker:"",imageURL: "assets/images/speakers/Bijay.jpg",desc:"Here we will discuss about  what is a workflow? What are different ways to develop workflow, how we can develop workflow using visual studio? How we can attach the workflow to host web?", time:"12.00 AM - 12.45 AM"},
         { title:"Lunch",imageURL:"assets/images/food.png", time:"1.00 PM - 2.00 PM"},
         { title:"To be added",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"02.00 PM - 02.45 PM"},
         { title:"To be added",speaker:"",imageURL: "assets/images/speakers/default-speaker.png",desc:"", time:"03.00 PM -3.45 PM"},
